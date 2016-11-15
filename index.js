5a00fcf607a0045cbe3e17fe3c57fd97bdb0e4f4
Edward FitzSimons <fitzs045@d.umn.edu>
2016-11-15 15:40:53 -0600

    Added more parks

--- index.js
+++ index.js
@@ -334,24 +334,37 @@ function updateDBTiles(){
     }
 }
 
-//Function creates empty grids when they don't already exist
-//Mongo implementation to be added
+/////////////////////////////////////////////////////////
+//      Functions to initialize grids
 function initGrids(){
 
     tileArray.tiles.push(initTileJSON);
+    addGrid(46.805993, -92.100449, 21, 13); //Chester Park
+    addGrid(46.820421, -92.091951, 11, 11); //Bagley Park
+    addGrid(46.825979, -92.100245, 17, 22); //Hunters Park
     
-    var lat = 46.805993;
-    var lang = -92.100449;
-    for(var i = 0; i < .0105; i += .0005){
-	for(var j = 0; j < .013; j += .001){
-
-	    var	tile = {
-		    lat: lat + i,
-		    lang: lang + j,
-		    status: -1};
-	    tileArray.tiles.push(tile);
-	}
-    }
+    addGrid(46.819225, -92.068734, 5, 5); //Congdon Park 1
+    addGrid(46.819225, -92.063734, 5, 1); //Congdon Park 2
+    addGrid(46.818725, -92.062734, 3, 3); //Congdon Park 3
+    addGrid(46.818225, -92.059734, 3, 3); //Congdon Park 4
+    addGrid(46.816225, -92.056734, 5, 3); //Congdon Park 5
 }
 
+/**
+* Function adds a grid to the tile array
+* @param strtLat Latitude of upper left grid corner
+* @param strtLang Longitude of upper left grid corner
+* @param width of grid
+* @param height of grid
+*/
+function initGridChester(strtLat, strtLang, width, height){
+    for(var i = 0; i < .0005 * width; i += .0005){
+	for(var j = 0; j < .001 * height; j += .001){
 
+	    var	tile = {
+		    lat: strtLat + i,
+		    lang: strtLang + j,
+		    status: -1};
+	    tileArray.tiles.push(tile);
+	}}
+}
